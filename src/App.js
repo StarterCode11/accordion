eb6fdcdef885d82cd68861ad58b45b06b4c3749b
StarterCode11 <infokumarpratyush03@gmail.com>
2023-11-07 21:21:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import questions from "./data";
+
+import Accordion from "./Accordion";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Accordion</h1>
+      {questions.map((question) => {
+        return <Accordion {...question} />;
+      })}
+    </div>
+  );
+}
